bd8f1d2e07da5065bcca31f2fce29cef407a87be
joyewon0705 <77885098+joyewon0705@users.noreply.github.com>
2024-10-28 16:15:07 +0900

    first commit

---
+++ src/test/java/com/example/alog/AlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
