7519e99709ab2640c716c42b2f5cd8971aba7766
SeobinYun <o0oseobin@naver.com>
2024-11-25 18:28:50 +0900

    checkout

--- src/main/java/com/example/alog/service/IssueService.java
+++ src/main/java/com/example/alog/service/IssueService.java
@@ -3,7 +3,6 @@
 package com.example.alog.service;
 
 import com.example.alog.entity.Issue;
-import com.example.alog.repository.IssueRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
